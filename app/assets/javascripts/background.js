32c7940f2f70d2c614132bd07b8deb907bb759a1
Ryan Tong <eufonik@hotmail.com>
2013-08-29 01:21:09 -0700

    fixing

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -3,5 +3,5 @@ $(document).ready(function() {
 	var backgrounds = ["bg_body_01.jpg", "bg_body_02.jpg", "bg_body_03.jpg", "bg_body_04.jpg", "bg_body_05.jpg", "bg_body_06.jpg", "bg_body_07.jpg"];
 	var randomInt = Math.floor(Math.random()*7);
 
-	$('body').css('background-image', 'image-url('+ "backgrounds[randomInt]" +')');
+	$('body').css('background-image', 'url('+ "backgrounds[randomInt]" +')');
 });
