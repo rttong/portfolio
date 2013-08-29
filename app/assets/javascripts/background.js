6c0881a082a69cbc0ab02da0b51e6ac05d04bd9f
Ryan Tong <eufonik@hotmail.com>
2013-08-29 01:06:39 -0700

    fu

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	var backgrounds = ["bg_body_01", "bg_body_02", "bg_body_03", "bg_body_04", "bg_body_05", "bg_body_06", "bg_body_07"];
+	var backgrounds = ["bg_body_01.jpg", "bg_body_02.jpg", "bg_body_03.jpg", "bg_body_04.jpg", "bg_body_05.jpg", "bg_body_06.jpg", "bg_body_07.jpg"];
 	var randomInt = Math.floor(Math.random()*7);
 
-	$('body').css('background-image', 'asset-url('+ backgrounds[randomInt] +'.jpg)');
+	$('body').css('background-image', 'asset-data-url('+ backgrounds[randomInt] +')');
 });
